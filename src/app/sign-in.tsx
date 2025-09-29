ac6cb63a915bdd379975da1b3357667f411568ae
bobby <deville75@gmail.com>
2025-09-29 10:22:16 +0200

    copy changes

--- src/app/sign-in.tsx
+++ src/app/sign-in.tsx
@@ -12,7 +12,7 @@ const SignIn = ({ onSignIn }: Props) => {
       }}
       className="w-full flex justify-center py-3 px-4 text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 dark:bg-blue-700 dark:hover:bg-blue-800 rounded-lg transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:focus:ring-blue-400 focus:ring-offset-white dark:focus:ring-offset-gray-800 hover:shadow-lg transform hover:-translate-y-0.5"
     >
-      Sign In with Logto
+      Sign In
     </button>
   );
 };
